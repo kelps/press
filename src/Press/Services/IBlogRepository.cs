964172caba0f5e3d1be9ab894918c341b0193757
Kelps Leite de Sousa <kelps@outlook.com>
2017-01-13 10:26:36 -0200

    initial test project

---
+++ src/Press/Services/IBlogRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Postal.Services {
+    public interface IBlogRepository {
+        IEnumerable<IPost> List();
+        void Save(IPost post);
+        void Delete(IPost post);
+
+        //void Delete(Post post);
+
+        //IEnumerable<Post> GetAll(string blogId);
+
+        //Dictionary<string, int> GetCategories(string blogId);
+
+        //Dictionary<string, int> GetTags(string blogId);
+
+        //void Save(Post post);
+
+        //string SaveMedia(string blogid, MediaObject media);
+
+    }
+}
---
+++ src/Press/Services/IPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Postal.Services
+{
+    public interface IPost
+    {
+        string Id { get; set; }
+        string Title { get; set; }
+        string Author { get; set; }
+        string Slug { get; set; }
+        string Excerpt { get; set;}
+        string Content { get; set; }
+        bool IsPublished { get; set; }
+        DateTime PublishDate { get; set; }
+        DateTime LastModified { get; set; }
+        IEnumerable<string> Categories { get; set; }
+        IEnumerable<string> Tags { get; set; }
+    }
+}
